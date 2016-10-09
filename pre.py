5282e3be99635c90c8c0d0cb7a33c2b7509c6575
Dream7hief <logan.poole1123@gmail.com>
2016-10-09 16:55:52 -0700

    Final Commit

--- pre.py
+++ pre.py
@@ -3,8 +3,10 @@ Test program for pre-processing schedule
 """
 import arrow
 
+global base
 base = arrow.now()
 
+CURRENT_DATE = arrow.now()##.format("ddd MM/DD/YYYY")
 def process(raw):
     """
     Line by line processing of syllabus file.  Each line that needs
@@ -26,14 +28,16 @@ def process(raw):
         if len(parts) == 2: 
             field = parts[0]
             content = parts[1]
+
         else:
             raise ValueError("Trouble with line: '{}'\n".format(line) + 
                 "Split into |{}|".format("|".join(parts)))
 
         if field == "begin":
             try:
+
                 base = arrow.get(content, "MM/DD/YYYY")
-                # print("Base date {}".format(base.isoformat()))
+                print("Base date {}".format(base.isoformat()))
             except:
                 raise ValueError("Unable to parse date {}".format(content))
 
@@ -44,10 +48,21 @@ def process(raw):
             entry['topic'] = ""
             entry['project'] = ""
             entry['week'] = content
+            
+            date = base.replace(weeks =+ int(content) - 1)
+            start_date = date.replace(days =- 2)
+            end_date = date.replace(days =+ 5)
+
+            
+            if (CURRENT_DATE >= start_date and CURRENT_DATE < end_date):
+                
+                entry['highlight'] = True
+            
+            entry['date'] = date.format("ddd MM/DD/YYYY")
 
         elif field == 'topic' or field == 'project':
             entry[field] = content
-
+            
         else:
             raise ValueError("Syntax error in line: {}".format(line))
 
